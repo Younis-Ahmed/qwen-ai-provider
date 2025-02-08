268ddb2ee853fb680d1aa99921663b73c97d9df2
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2025-02-08 17:40:39 +0300

    chore: add Vitest configuration for edge and node environments

---
+++ vitest.edge.config.js
@@ -0,0 +1,13 @@
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    environment: 'edge-runtime',
+    globals: true,
+    include: ['**/*.test.ts', '**/*.test.tsx'],
+    reporters: ['text', 'json', 'html'],
+  },
+})
